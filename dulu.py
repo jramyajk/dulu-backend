f2c2de4aed6aa4690fe4892a837ca110e68ed193
jramyajk <jramya0207@gmail.com>
2026-01-04 22:27:38 +0530

    Fix Railway port

--- dulu.py
+++ dulu.py
@@ -1,21 +1,21 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
 
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(_file_))
+FRONTEND_DIR = os.path.join(BASE_DIR, "../frontend")
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+app = Flask(_name_, static_folder=FRONTEND_DIR, template_folder=FRONTEND_DIR)
 
 @app.route("/")
 def home():
-    return send_from_directory("../frontend", "index.html")
+    return send_from_directory(FRONTEND_DIR, "index.html")
 
 @app.route("/sos", methods=["POST"])
 def sos():
     data = request.get_json()
     print("🚨 SOS TRIGGERED:", data)
     return jsonify({"status": "sent"})
-import os
 
-if __name__ == "__main__":
-port = int(os.environ.get("port",5000))
-    app.run(host="0.0.0.0",port=port)
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
