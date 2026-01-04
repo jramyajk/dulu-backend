7c71511c52007fb85206ae78e50396ce77fd2f88
jramyajk <jramya0207@gmail.com>
2026-01-04 22:37:20 +0530

    Fix Railway port

--- dulu.py
+++ dulu.py
@@ -17,5 +17,5 @@ def sos():
     return jsonify({"status": "sent"})
 
 if _name_ == "_main_":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
